b19ceafcf2a049c4eb7aab7cfbbc61a6eaf4131b
adam.byford <adambyf1324.5@gmail.com>
2026-07-05 14:05:57 +0200

    order feature - done

--- gameShop/src/main/java/org/example/web/OrderController.java
+++ gameShop/src/main/java/org/example/web/OrderController.java
@@ -1,21 +1,21 @@
 package org.example.web;
 
 import lombok.RequiredArgsConstructor;
-import org.example.repositories.CartRepository;
+import org.example.dto.OrderResponse;
 import org.example.repositories.UserRepository;
 import org.example.services.OrderServiceInterface;
-import org.example.services.impl.CartService;
-import org.example.services.impl.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderServiceInterface orderService;
-    private final CartRepository cartRepository;
     private final UserRepository userRepository;
 
     private String getLoggedUserId(Authentication authentication) {
@@ -25,5 +25,27 @@ public class OrderController {
                 .getId();
     }
 
+    @PostMapping("/checkout")
+    public ResponseEntity<Void> placeOrder(Authentication authentication) {
+        String userId = getLoggedUserId(authentication);
 
+        orderService.checkout(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/my-order-history")
+    public ResponseEntity<List<OrderResponse>> getOrders(Authentication authentication) {
+        String userId = getLoggedUserId(authentication);
+        List<OrderResponse> orders = orderService.seeOrderHistory(userId);
+
+        return ResponseEntity.ok(orders);
+    }
+
+    @PatchMapping("/{orderId}/status")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> updateOrderStatus(@PathVariable("orderId") String orderId, @RequestParam String newStatus) {
+        orderService.changeOrderStatus(orderId, newStatus);
+
+        return ResponseEntity.ok().build();
+    }
 }
